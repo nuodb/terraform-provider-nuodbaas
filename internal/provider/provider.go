2c4a0da38266f575f7ff922a1a49eea28f7c714f
Adrian Suarez <asz6@3ds.com>
2024-02-01 21:30:59 -0500

    Basic scaffolding using provider generator
    Following instructions in https://developer.hashicorp.com/terraform/plugin/code-generation/workflow-example

---
+++ internal/provider/provider.go
@@ -0,0 +1,39 @@
+package provider
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/provider"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+)
+
+var _ provider.Provider = (*nuodbaasProvider)(nil)
+
+func New() func() provider.Provider {
+	return func() provider.Provider {
+		return &nuodbaasProvider{}
+	}
+}
+
+type nuodbaasProvider struct{}
+
+func (p *nuodbaasProvider) Schema(ctx context.Context, req provider.SchemaRequest, resp *provider.SchemaResponse) {
+
+}
+
+func (p *nuodbaasProvider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
+
+}
+
+func (p *nuodbaasProvider) Metadata(ctx context.Context, req provider.MetadataRequest, resp *provider.MetadataResponse) {
+	resp.TypeName = "nuodbaas"
+}
+
+func (p *nuodbaasProvider) DataSources(ctx context.Context) []func() datasource.DataSource {
+	return []func() datasource.DataSource{}
+}
+
+func (p *nuodbaasProvider) Resources(ctx context.Context) []func() resource.Resource {
+	return []func() resource.Resource{}
+}
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+    "context"
+    "log"
+
+    "terraform-provider-nuodbaas/internal/provider"
+
+    "github.com/hashicorp/terraform-plugin-framework/providerserver"
+)
+
+func main() {
+    opts := providerserver.ServeOpts{
+        Address: "registry.terraform.io/nuodb/nuodbaas",
+    }
+
+    err := providerserver.Serve(context.Background(), provider.New(), opts)
+    if err != nil {
+        log.Fatal(err.Error())
+    }
+}
