8f0dcaaf3c13c30e3136a8b6d51770a488ff58dd
Adrian Suarez <3846392+adriansuarez@users.noreply.github.com>
2024-02-10 10:43:41 -0500

    Add CircleCI job using REST service backed by CRUD-only K8s (#11)
    This change adds a CircleCI workflow that uses the test-helper package
    that has been published to nuodb-cp-releases, to allow the tests to run
    much faster and enable us to increase test coverage.
    
    The test-helper package includes the NuoDB Control Plane REST service
    and code that allows a CRUD-only Kubernetes cluster to be created along
    with controllers that simulate reconciliation by the NuoDB Operator.
    
    The test themselves take only about 10 seconds to run in this
    configuration, compared to about five minutes for the existing
    end-to-end configuration.

---
+++ .circleci/setup.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+set -ex
+
+# Create test results and output directories
+mkdir -p "$TEST_RESULTS"
+mkdir -p "$OUTPUT_DIR"
+
+# Download kubectl
+curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v"${KUBERNETES_VERSION}"/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
+
+if [ "$E2E_TEST" = true ]; then
+    # Download minikube
+    curl -Lo minikube https://storage.googleapis.com/minikube/releases/v"${MINIKUBE_VERSION}"/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
+
+    # Start minikube
+    minikube start --vm-driver=docker --kubernetes-version=v"${KUBERNETES_VERSION}"
+    minikube status
+    kubectl cluster-info
+
+    nohup minikube tunnel > "${OUTPUT_DIR}/minikube_tunnel.log" 2>&1 &
+
+    # Install helm
+    curl -Lo /tmp/helm.tar.gz https://get.helm.sh/helm-"${HELM_VERSION}"-linux-amd64.tar.gz
+    tar xzf /tmp/helm.tar.gz -C /tmp --strip-components=1 && chmod +x /tmp/helm && sudo mv /tmp/helm /usr/local/bin
+
+    # Install Cert-manager, Nginx Ingress, and the Control Plane into K8s cluster
+    make deploy-cp
+
+    # Extract credentials and make them available as environment variables for subsequent steps in job
+    make extract-creds >> "$BASH_ENV"
+else
+    # Download test helper for REST service that includes CRUD-only K8s and mock operators
+    curl -Lo test-helper.tgz https://github.com/nuodb/nuodb-cp-releases/releases/download/test-helper/test-helper.tgz
+    tar -xf test-helper.tgz
+
+    # Start K8s, mock operators, and REST service
+    MARK_AS_READY=true ./test-helper/setup-rest.sh
+
+    # Allow kubectl to be used against cluster by subsequent commands if needed
+    echo "export KUBECONFIG=\"$OUTPUT_DIR/kubeconfig.yml\"" >> "$BASH_ENV"
+fi
