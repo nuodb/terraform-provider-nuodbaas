78a4f1259ef768887ac45741ee1adf59358b6cad
Adrian Suarez <asz6@3ds.com>
2024-03-05 20:49:24 -0500

    Randomize project name in app test to avoid collisions
    This change randomizes the project name so that collisions do not occur
    if two tests runs execute concurrently.

--- test/app/nuodb.tf
+++ test/app/nuodb.tf
@@ -1,6 +1,12 @@
+# Generate a random suffix to avoid collisions
+resource "random_integer" "suffix" {
+  min = 1000
+  max = 9999
+}
+
 resource "nuodbaas_project" "test" {
   organization = var.org_name
-  name         = var.project_name
+  name         = "${var.project_name}${random_integer.suffix.id}"
   sla          = "test"
   tier         = "n0.nano"
 }
