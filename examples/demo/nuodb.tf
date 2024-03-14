00644d9b2eff4129d935525c689553271c659f6c
Adrian Suarez <asz6@3ds.com>
2024-03-14 14:12:42 -0400

    Add variable for dba_password

--- examples/demo/nuodb.tf
+++ examples/demo/nuodb.tf
@@ -22,7 +22,15 @@ resource "nuodbaas_database" "db" {
   organization = nuodbaas_project.proj.organization
   project      = nuodbaas_project.proj.name
   name         = "demo"
-  dba_password = "changeIt"
+  dba_password = var.dba_password
+}
+
+variable "dba_password" {
+  description = "The password for the DBA user"
+  type        = string
+  sensitive   = true
+  # Default value is specified for convenience and is not a best practice
+  default = "changeIt"
 }
 
 # Expose nuosql arguments to connect to database
