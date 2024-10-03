ab5e61ef23ef23e9dbc476d3c0566754e82b1dce
Stanimir Ivanov <siv3@3ds.com>
2024-10-03 12:52:38 +0300

    Fix previous log collection

--- deploy/k8s/logs.sh
+++ deploy/k8s/logs.sh
@@ -10,7 +10,7 @@ if [ -d "$OUTPUT_DIR" ]; then
         kubectl get pod -o jsonpath="{range .items[*]}{.metadata.name}{' '}{.status.containerStatuses[?(.restartCount > 0)].name}{'\n'}{end}"
     ) | while read pod containers; do
         for container in $containers; do
-            kubectl logs "$pod" -c "$container" > "$OUTPUT_DIR/cluster-info/$ns/$pod/$container-previous.txt"
+            kubectl logs "$pod" -c "$container" -p > "$OUTPUT_DIR/cluster-info/$ns/$pod/$container-previous.txt"
         done
     done
 else
