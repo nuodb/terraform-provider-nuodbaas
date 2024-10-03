0809af3de878b67a5048ffacc6631552df45ae38
Adrian Suarez <3846392+adriansuarez@users.noreply.github.com>
2024-10-03 08:27:00 -0400

    Collect logging for exited containers (#70)
    For any containers with non-0 restart counts, collect logging for exited
    containers using `kubectl logs ... --previous`.

--- deploy/k8s/logs.sh
+++ deploy/k8s/logs.sh
@@ -3,6 +3,16 @@
 if [ -d "$OUTPUT_DIR" ]; then
     mkdir -p "$OUTPUT_DIR/cluster-info"
     kubectl cluster-info dump -A --output-directory="$OUTPUT_DIR/cluster-info"
+
+    # For any containers that have restarted, collect the exited container output
+    (
+        kubectl get pod -A -o jsonpath="{range .items[*]}{.metadata.namespace}{' '}{.metadata.name}{' '}{.status.containerStatuses[?(.restartCount > 0)].name}{'\n'}{end}"
+    ) | while read ns pod containers; do
+        for container in $containers; do
+            kubectl logs -n "$ns" "$pod" -c "$container" --previous > "$OUTPUT_DIR/cluster-info/$ns/$pod/logs-$container-previous.txt"
+        done
+    done
 else
     kubectl cluster-info dump -A
 fi
+
